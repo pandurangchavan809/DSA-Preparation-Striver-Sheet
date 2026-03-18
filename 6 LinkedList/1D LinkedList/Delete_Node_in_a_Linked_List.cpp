375c585ea730594bee2ccb48e9cf4e5ad36a8215
pandurangchavan809 <pandurangchavan809@gmail.com>
2026-03-19 00:08:05 +0530

    delete 1d array node

--- 6 LinkedList/1D LinkedList/Delete_Node_in_a_Linked_List.cpp
+++ 6 LinkedList/1D LinkedList/Delete_Node_in_a_Linked_List.cpp
@@ -1,4 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// 
+// tc - O(N) and Sc- O(1)
+// delete the node in the linked list 
+
+// class Solution {
+// public:
+//     void deleteNode(ListNode* node) {
+//         node->val = node->next->val;     // copy next node value
+//         node->next = node->next->next;   // skip next node
+//     }
+// };
