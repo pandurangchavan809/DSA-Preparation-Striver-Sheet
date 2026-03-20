cbfbaf1486caf6a938032ec2207893ce4e524bb9
pandurangchavan809 <pandurangchavan809@gmail.com>
2026-03-21 02:24:15 +0530

    recursive method to reverse a linked list

---
+++ 6 LinkedList/medium_problem_of_LL/recursive_reverse_LL.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Recursive Reverse a LinkedList :
+
+struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode() : val(0), next(nullptr) {}
+   ListNode(int x) : val(x), next(nullptr) {}
+   ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return newHead;
+    }
+};
+
+
+// Time Complexity: O(n),Each node is visited exactly once during the recursive call, and we do constant-time work for each node (like flipping pointers).
+
+// Space Complexity: O(n),The recursion stack goes up to n levels deep (one for each node), which uses extra space on the call stack.
