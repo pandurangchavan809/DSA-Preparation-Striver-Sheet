d61bf450f476a1246b7797ca5c78337ec0d004b6
pandurangchavan809 <pandurangchavan809@gmail.com>
2026-03-22 23:49:05 +0530

    recursive_reverse_ll

--- 6 LinkedList/medium_problem_of_LL/recursive_reverse_LL.cpp
+++ 6 LinkedList/medium_problem_of_LL/recursive_reverse_LL.cpp
@@ -27,4 +27,4 @@ public:
 
 // Time Complexity: O(n).....Each node is visited exactly once during the recursive call, and we do constant-time work for each node (like flipping pointers).
 
-// Space Complexity: O(n),The recursion stack goes up to n levels deep (one for each node), which uses extra space on the call stack.
+// Space Complexity: O(n).....The recursion stack goes up to n levels deep (one for each node), which uses extra space on the call stack.
