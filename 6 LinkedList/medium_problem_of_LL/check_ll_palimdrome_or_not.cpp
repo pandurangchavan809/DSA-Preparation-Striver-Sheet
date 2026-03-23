d894877b66aa7acdd241962baa03cf542f5444c7
pandurangchavan809 <pandurangchavan809@gmail.com>
2026-03-23 23:43:34 +0530

    segregate even odd nodes in LL

--- 6 LinkedList/medium_problem_of_LL/check_ll_palimdrome_or_not.cpp
+++ 6 LinkedList/medium_problem_of_LL/check_ll_palimdrome_or_not.cpp
@@ -9,13 +9,11 @@ struct ListNode {
    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-
 // Check if a LinkedList is Palindrome or Not  :
 
 // Brute force:- take an stack put all values of list by tranversing using temp=head to last then again start transver and compare with stack top values if match pop and check for next one until stack if empty....if palimdrome all values equally matches
 
 // Optimal Solution :
-
 
 ListNode* reverseLinkedList(ListNode* head){
     if(head == NULL || head->next == NULL) return head;
@@ -25,8 +23,6 @@ ListNode* reverseLinkedList(ListNode* head){
     head->next=NULL;
     return newHead;
 }
-
-
 
 bool isPalindrome(ListNode* head){
     if(head==NULL || head->next == NULL)return true;
@@ -53,7 +49,8 @@ bool isPalindrome(ListNode* head){
     return true;
 }
 
+
+
 // Time Complexity: O(N), we traverse the list twice, once to reverse half of it and once to compare, each taking O(N/2), which simplifies to O(N).
 
 // Space Complexity: O(1), no extra space is used apart from a few pointers; operations are done in-place.
-
---
+++ 6 LinkedList/medium_problem_of_LL/segregate_even_odd_nodes_LL.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Segregate even and odd nodes in LinkedList :
+
+
+
+
+
+
+
+
+
+
+
+
+// Time Complexity: O(n), We traverse the entire linked list only once to rearrange the nodes. Each node is visited exactly once. No nested traversal or re-traversal occurs. Hence, linear time in terms of the number of nodes n.
+
+// Space Complexity: O(1), We do not use any extra data structures
