763b2590a387c4965e70ada2c54d8b25d6f2f01b
pandurangchavan809 <pandurangchavan809@gmail.com>
2026-04-06 21:55:55 +0530

    palindrome partioning

--- 7 Recursion/trying_out_all_combos/hard/word_break.cpp
+++ 7 Recursion/trying_out_all_combos/hard/word_break.cpp
@@ -2,4 +2,37 @@
 using namespace std;
 
 // Word Break 
+// Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
 
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+// Input: s = "leetcode", wordDict = ["leet","code"]
+// Output: true
+// Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> st(wordDict.begin(), wordDict.end());
+
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+
+        dp[0] = true; // empty string
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(dp[j] && st.find(s.substr(j, i - j)) != st.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+// Complexity
+// Time: O(n²)  ...if optimizes
+// Space: O(n)
