f8826c06e3804866430593a8dbb1f3999db4fba5
pandurangchavan809 <pandurangchavan809@gmail.com>
2026-05-09 14:51:30 +0530

    greedy algorithm

--- Greedy Algorithm/medium/insert_interval.cpp
+++ Greedy Algorithm/medium/insert_interval.cpp
@@ -1,13 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Insert Interval :
-
+// Insert Interval : 
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        
+        vector<vector<int>> res;  int i=0;
+        int n=intervals.size();
+
+        // left part
+
+        while(i<n && intervals[i][1] < newInterval[0]){
+            res.push_back(intervals[i]);
+            i=i+1;
+        }
+
+        // collision part
+
+        while(i<n && intervals[i][0] <= newInterval[1]){
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1], intervals[i][1]);
+            i=i+1;
+        }
+        res.push_back(newInterval);
+
+        // right part
+
+        while(i<n){
+            res.push_back(intervals[i]);
+            i=i+1;
+        }
+
+        return res;
     }
 };
 
 // Time compplexity: O(N)  && space complexity : O(N)
+
